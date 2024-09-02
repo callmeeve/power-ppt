315f43da9e093267587feff409552de30f24b1ec
callmeeve <wididiky38@gmail.com>
2024-09-02 15:33:33 +0700

    commit

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,21 +1,10 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
-// import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
-import { Bell, ChevronDown, LayoutGrid, Menu, Settings, User, X, Folder, File  } from 'lucide-react'
-
+import { Bell, ChevronDown, LayoutGrid, Menu, User, X, Folder, File  } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
-
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -24,18 +13,6 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-
-// import { Input } from "@/components/ui/input"
-
-// const data = [
-//   { name: 'Jan', uv: 4000, pv: 2400, amt: 2400 },
-//   { name: 'Feb', uv: 3000, pv: 1398, amt: 2210 },
-//   { name: 'Mar', uv: 2000, pv: 9800, amt: 2290 },
-//   { name: 'Apr', uv: 2780, pv: 3908, amt: 2000 },
-//   { name: 'May', uv: 1890, pv: 4800, amt: 2181 },
-//   { name: 'Jun', uv: 2390, pv: 3800, amt: 2500 },
-// ]
-
 
 export default function AdminLayout({
     children,
@@ -124,108 +101,6 @@ export default function AdminLayout({
                 <main className="flex-1 overflow-x-hidden overflow-y-auto bg-white">
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         {children}
-                        {/* <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-                            <Card>
-                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">
-                                        Total Revenue
-                                    </CardTitle>
-                                    <DollarSign className="h-4 w-4 text-muted-foreground" />
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="text-2xl font-bold">$45,231.89</div>
-                                    <p className="text-xs text-muted-foreground">
-                                        +20.1% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">
-                                        New Users
-                                    </CardTitle>
-                                    <User className="h-4 w-4 text-muted-foreground" />
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="text-2xl font-bold">+2350</div>
-                                    <p className="text-xs text-muted-foreground">
-                                        +180.1% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                                    <CreditCard className="h-4 w-4 text-muted-foreground" />
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="text-2xl font-bold">+12,234</div>
-                                    <p className="text-xs text-muted-foreground">
-                                        +19% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                    <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-                                    <User className="h-4 w-4 text-muted-foreground" />
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="text-2xl font-bold">+573</div>
-                                    <p className="text-xs text-muted-foreground">
-                                        +201 since last hour
-                                    </p>
-                                </CardContent>
-                            </Card>
-                        </div>
-
-                        <div className="grid gap-6 mb-8 md:grid-cols-2">
-                            <Card className="col-span-2">
-                                <CardHeader>
-                                    <CardTitle>Revenue Overview</CardTitle>
-                                </CardHeader>
-                                <CardContent className="p-0">
-                                    <ResponsiveContainer width="100%" height={300}>
-                                        <BarChart data={data}>
-                                            <CartesianGrid strokeDasharray="3 3" />
-                                            <XAxis dataKey="name" />
-                                            <YAxis />
-                                            <Tooltip />
-                                            <Legend />
-                                            <Bar dataKey="pv" fill="#8884d8" />
-                                            <Bar dataKey="uv" fill="#82ca9d" />
-                                        </BarChart>
-                                    </ResponsiveContainer>
-                                </CardContent>
-                            </Card>
-                        </div>
-
-                        <div className="mt-8">
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Recent Transactions</CardTitle>
-                                    <CardDescription>You made 265 sales this month.</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className="space-y-8">
-                                        
-                                        {[1, 2, 3].map((item) => (
-                                            <div key={item} className="flex items-center">
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        Customer Name {item}
-                                                    </p>
-                                                    <p className="text-sm text-muted-foreground">
-                                                        customer{item}@example.com
-                                                    </p>
-                                                </div>
-                                                <div className="ml-auto font-medium">+$1,999.00</div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </CardContent>
-                            </Card>
-                        </div> */}
                     </div>
                 </main>
             </div>
